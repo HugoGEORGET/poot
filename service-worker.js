7e10d106eaa4c242b80962c4ff11e5f2378f4027
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-07 16:09:58 +0000

    Deploy HugoGEORGET/poot to github.com/HugoGEORGET/poot.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84a277f19559a75cdf3f50b8bcc91e93.js"
+  "/poot/precache-manifest.b096228454f24f32d8dd117025f4c8aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
