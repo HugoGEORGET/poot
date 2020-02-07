d7464f17554d90fd007486a7bb8a1f5a57b833bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-07 16:06:09 +0000

    Deploy HugoGEORGET/poot to github.com/HugoGEORGET/poot.git:gh-pages

---
+++ precache-manifest.84a277f19559a75cdf3f50b8bcc91e93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b80b7f37f2c3dd216a74fad2b1660ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a8d208a3d0fd9aee58a",
+    "url": "/static/css/main.f31eb386.chunk.css"
+  },
+  {
+    "revision": "201924c56ee230325d8b",
+    "url": "/static/js/2.645d252f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.645d252f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8d208a3d0fd9aee58a",
+    "url": "/static/js/main.eca9e725.chunk.js"
+  },
+  {
+    "revision": "3e3091253e017ff11994",
+    "url": "/static/js/runtime-main.c20f869d.js"
+  },
+  {
+    "revision": "39b61928fe5c1ba37afea4a10da59c63",
+    "url": "/static/media/poootis.39b61928.png"
+  },
+  {
+    "revision": "2cbecf96b1dee45dc866337217522564",
+    "url": "/static/media/pootis.2cbecf96.png"
+  }
+]);
