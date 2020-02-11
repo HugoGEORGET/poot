8b9e237a0b602044946770e0488c33992df7fb4f
Hugo Georget <hugo.georget@sword-group.com>
2020-02-11 14:57:03 +0100

    :sparkles: Canvas update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    let canvas = document.getElementById("canvas");
+
+    if (canvas instanceof HTMLCanvasElement) {
+      let canvasContext = canvas.getContext("2d");
+
+      if (canvasContext instanceof CanvasRenderingContext2D) {
+        canvasContext.canvas.width = window.innerWidth;
+        canvasContext.canvas.height = window.innerHeight;
+
+        let addImage = (event: MouseEvent) => {
+          console.log("addImage");
+          let x = event.clientX;
+          let y = event.clientY;
+
+          let pootImage = new Image();
+          pootImage.src = "./pootis.png";
+          pootImage.onload = () => {
+            canvasContext?.drawImage(pootImage, x, y);
+          };
+        };
+
+        canvas.addEventListener("mousedown", addImage);
+      }
+    }
+  }, []);
+
   function playAudio() {
     if (document.getElementById("poot")) {
       let pootAudio = document.getElementById("poot");
@@ -15,6 +42,7 @@ const App = () => {
   return (
     <div className="App" onClick={() => playAudio()}>
       <audio id="poot" preload="auto" src="./poot.mp3"></audio>
+      <canvas id="canvas"></canvas>
     </div>
   );
 };
