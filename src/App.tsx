3838394521565dc2544907c4bc124d4574f822b1
Hugo Georget <hugo.georget@protonmail.com>
2026-03-29 17:49:42 +0200

    Migrate to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
 const addImageToCanvas = (event: MouseEvent, canvasContext: CanvasRenderingContext2D) => {
@@ -6,43 +6,59 @@ const addImageToCanvas = (event: MouseEvent, canvasContext: CanvasRenderingConte
   let y = event.clientY;
 
   let pootImage = new Image();
-  pootImage.src = "./pootis.png";
+  pootImage.src = "pootis.png";
   pootImage.onload = () => {
     canvasContext?.drawImage(pootImage, x - 50, y - 50);
   };
 };
 
-const pootSound = new Audio("./poot.mp3");
+const pootSound = new Audio("poot.mp3");
 const onMouseDownFunc = (event: MouseEvent, canvasContext: CanvasRenderingContext2D) => {
   addImageToCanvas(event, canvasContext);
   pootSound.play();
 };
 
 const App = () => {
-  const canvas = document.getElementById("canvas");
-
   useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    let canvasContext: CanvasRenderingContext2D;
+
     if (canvas instanceof HTMLCanvasElement) {
-      const canvasContext = canvas.getContext("2d");
+      const ctx = canvas.getContext("2d");
 
-      if (canvasContext instanceof CanvasRenderingContext2D) {
-        canvasContext.canvas.width = window.innerWidth;
-        canvasContext.canvas.height = window.innerHeight;
+      if (ctx) {
+        canvasContext = ctx // non-null assignment
 
-        canvas.addEventListener("mousedown", (event) => onMouseDownFunc(event, canvasContext));
-        window.addEventListener("resize", () => {
-          if (canvasContext instanceof CanvasRenderingContext2D) {
+        if (canvasContext && canvasContext !== null) {
+          canvasContext.canvas.width = window.innerWidth;
+          canvasContext.canvas.height = window.innerHeight;
+
+          canvas.addEventListener("mousedown", (event) => onMouseDownFunc(event, canvasContext));
+
+          // Handle window resize (change canvas width to window width)
+          window.addEventListener("resize", () => {
             canvasContext.canvas.width = window.innerWidth;
             canvasContext.canvas.height = window.innerHeight;
-          }
+          });
+        }
+      }
+    }
+
+    return () => {
+      if (canvasContext) {
+        canvas?.removeEventListener('mousedown', (event) => onMouseDownFunc(event, canvasContext))
+
+        window.removeEventListener("resize", () => {
+          canvasContext.canvas.width = window.innerWidth;
+          canvasContext.canvas.height = window.innerHeight;
         });
       }
     }
-  }, [canvas]);
+  }, []);
 
   return (
     <div className="App">
-      <canvas id="canvas"></canvas>
+      <canvas id="canvas" />
     </div>
   );
 };
