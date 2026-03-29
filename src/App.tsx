fe63b596d269aa25c9c941f4c96ba3dbc0940385
Hugo Georget <hugo.georget@protonmail.com>
2026-03-29 17:05:14 +0200

    Update React

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
 import React, { useEffect } from "react";
 import "./App.css";
 
-const App = () => {
-  useEffect(() => {
-    let canvas = document.getElementById("canvas");
+const addImageToCanvas = (event: MouseEvent, canvasContext: CanvasRenderingContext2D) => {
+  let x = event.clientX;
+  let y = event.clientY;
 
+  let pootImage = new Image();
+  pootImage.src = "./pootis.png";
+  pootImage.onload = () => {
+    canvasContext?.drawImage(pootImage, x - 50, y - 50);
+  };
+};
+
+const pootSound = new Audio("./poot.mp3");
+const onMouseDownFunc = (event: MouseEvent, canvasContext: CanvasRenderingContext2D) => {
+  addImageToCanvas(event, canvasContext);
+  pootSound.play();
+};
+
+const App = () => {
+  const canvas = document.getElementById("canvas");
+
+  useEffect(() => {
     if (canvas instanceof HTMLCanvasElement) {
-      let canvasContext = canvas.getContext("2d");
+      const canvasContext = canvas.getContext("2d");
 
       if (canvasContext instanceof CanvasRenderingContext2D) {
         canvasContext.canvas.width = window.innerWidth;
         canvasContext.canvas.height = window.innerHeight;
 
-        let addImage = (event: MouseEvent) => {
-          let x = event.clientX;
-          let y = event.clientY;
-
-          let pootImage = new Image();
-          pootImage.src = "./pootis.png";
-          pootImage.onload = () => {
-            canvasContext?.drawImage(pootImage, x - 50, y - 50);
-          };
-        };
-
-        let playPootSound = () => {
-          let pootSound = new Audio("./poot.mp3");
-          pootSound.play();
-        };
-
-        let mousedownEvent = (event: MouseEvent) => {
-          addImage(event);
-          playPootSound();
-        };
-
-        canvas.addEventListener("mousedown", mousedownEvent);
+        canvas.addEventListener("mousedown", (event) => onMouseDownFunc(event, canvasContext));
         window.addEventListener("resize", () => {
           if (canvasContext instanceof CanvasRenderingContext2D) {
             canvasContext.canvas.width = window.innerWidth;
@@ -42,7 +38,7 @@ const App = () => {
         });
       }
     }
-  }, []);
+  }, [canvas]);
 
   return (
     <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const domNode = document.getElementById('root')
+
+if (domNode) {
+    const root = createRoot(domNode)
+    root.render(<App />)
+} else {
+    console.warn("No element found with id 'root'")
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
